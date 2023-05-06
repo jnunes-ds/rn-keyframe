ce9d7c9e9241b3e778ed63bf28b31718fbf2e613
Júnior Nunes <denisonprobable13@gmail.com>
2023-05-06 14:38:56 -0300

    feat: using Flip animation

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+import Animated, { FlipInEasyY, FlipOutEasyY } from "react-native-reanimated";
 
 import S from "./Message.styles";
 
@@ -9,8 +9,8 @@ export default function Message() {
   return (
     <Animated.View
       style={S.container}
-      entering={FadeIn.duration(700)}
-      exiting={FadeOut.duration(700)}
+      entering={FlipInEasyY.duration(700)}
+      exiting={FlipOutEasyY.duration(700)}
     >
       <MaterialIcons name="notifications" color="#FFF" size={18} />
       <Text style={S.title}>Hello World</Text>
