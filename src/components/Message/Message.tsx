7c6a024c5843a9b87c04e81ea5c44c06c3d76caa
Júnior Nunes <denisonprobable13@gmail.com>
2023-05-06 14:29:55 -0300

    feat: using pre-fab animations to show and hide message

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
 
 import S from "./Message.styles";
 
 export default function Message() {
   return (
-    <View style={S.container}>
+    <Animated.View
+      style={S.container}
+      entering={FadeIn.duration(700)}
+      exiting={FadeOut.duration(700)}
+    >
       <MaterialIcons name="notifications" color="#FFF" size={18} />
       <Text style={S.title}>Hello World</Text>
-    </View>
+    </Animated.View>
   );
 }
