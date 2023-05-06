7dfb6506c5a24be771b177ea14e4bf4b2e440a08
Júnior Nunes <denisonprobable13@gmail.com>
2023-05-06 14:45:50 -0300

    feat: using keyframe for real

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import { Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import Animated, { FlipInEasyY, FlipOutEasyY } from "react-native-reanimated";
+import Animated, { Keyframe } from "react-native-reanimated";
 
 import S from "./Message.styles";
 
 export default function Message() {
+  const enteringKeyframe = new Keyframe({
+    0: {
+      opacity: 0,
+      transform: [
+        {
+          translateY: -100,
+        },
+      ],
+    },
+    70: {
+      opacity: 0.3,
+    },
+    100: {
+      opacity: 1,
+      transform: [
+        {
+          translateY: 0,
+        },
+      ],
+    },
+  });
+
   return (
     <Animated.View
       style={S.container}
-      entering={FlipInEasyY.duration(700)}
-      exiting={FlipOutEasyY.duration(700)}
+      entering={enteringKeyframe.duration(500)}
     >
       <MaterialIcons name="notifications" color="#FFF" size={18} />
       <Text style={S.title}>Hello World</Text>
