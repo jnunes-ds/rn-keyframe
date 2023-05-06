9ed2fc3941fe55c6ec9952887e92bf9472a19b99
Júnior Nunes <denisonprobable13@gmail.com>
2023-05-06 19:45:38 -0300

    feat: creating new drawing with skia

--- src/screens/SkiaScreen/SkiaScreen.tsx
+++ src/screens/SkiaScreen/SkiaScreen.tsx
@@ -1,16 +1,33 @@
 import React from "react";
-import { Canvas, Circle, center } from "@shopify/react-native-skia";
+import {
+  Canvas,
+  Circle,
+  center,
+  Oval,
+  rect,
+  Group,
+  Rect,
+} from "@shopify/react-native-skia";
 
 import S from "./SkiaScreen.styles";
 
 function SkiaScreen() {
+  const color = "#6358f5";
+
+  const centerArgs = { height: 300, width: 350, x: 25, y: 250 };
+
   return (
     <Canvas style={S.container}>
-      <Circle
-        c={center({ height: 300, width: 350, x: 25, y: 250 })}
-        r={25}
-        color="#6358f5"
-      />
+      <Circle c={center(centerArgs)} r={25} color={color} />
+      <Group color={color} style="stroke" strokeWidth={18}>
+        <Oval rect={rect(50, 350, 300, 100)} />
+        <Group transform={[{ rotate: Math.PI / 3 }]} origin={centerArgs}>
+          <Oval rect={rect(100, 125, 300, 100)} />
+        </Group>
+        <Group transform={[{ rotate: -Math.PI / 3 }]} origin={centerArgs}>
+          <Oval rect={rect(-175, 425, 300, 100)} />
+        </Group>
+      </Group>
     </Canvas>
   );
 }
